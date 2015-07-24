db6f2d24f0d37426980176d8b3d09de460a19984
moon-sky <511014405@qq.com>
2015-07-24 14:17:10 +0800

    first commit

---
+++ src/com/pstreets/guice/demo/GreetingModule.java
@@ -0,0 +1,16 @@
+package com.pstreets.guice.demo;
+
+import com.google.inject.AbstractModule;
+import com.pstreets.guice.demo.service.HelloChina;
+import com.pstreets.guice.demo.service.IGreetingService;
+
+public class GreetingModule extends AbstractModule {
+
+	@Override
+	protected void configure() {
+//		bind(IGreetingService.class).to(HelloWorld.class);
+		bind(IGreetingService.class).to(HelloChina.class);
+		
+	}
+
+}
